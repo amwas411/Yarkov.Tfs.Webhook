91bc8f972ff99ca0fff7d3854154f8dee2150a2c
amwas411 <yarkovpmi@gmail.com>
2026-09-10 14:31:22 +0300

    Add AiController

--- Yarkov.Tfs.Webhook.Test/Test1.cs
+++ Yarkov.Tfs.Webhook.Test/Test1.cs
@@ -1,13 +1,22 @@
-﻿using System.Diagnostics;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Windows.Markup;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Yarkov.Tfs.Webhook.Models;
 using Yarkov.Tfs.Webhook.Storage;
 
 namespace Yarkov.Tfs.Webhook.Test;
+
+public static class TestConstants
+{
+  public static string LogFileName = "log.txt";
+  public static string TerminalRunner = "E:/Programs SSD/Git/bin/bash.exe";
+  public static string WorkDirectoryName = "data";
+  public static string AiWorkDirectoryName = "aidata";
+  public static string TimesheetFileName = "timesheet.csv";
+  public static bool AiDiagnosticMode = true;
+  public static string AiName = "$ревью";
+}
 
 [TestClass]
 public sealed class Test1
@@ -22,6 +31,16 @@ public sealed class Test1
     var c = new ServiceCollection();
     c.AddSingleton<IFileStorage, FileStorage>();
     c.AddTransient<ILogger, Logger>();
+    c.AddOptions<AppOptions>().Configure((appOptions) =>
+    {
+      appOptions.LogFileName = TestConstants.LogFileName;
+      appOptions.TerminalRunner = TestConstants.TerminalRunner;
+      appOptions.WorkDirectoryName = TestConstants.WorkDirectoryName;
+      appOptions.AiWorkDirectoryName = TestConstants.AiWorkDirectoryName;
+      appOptions.TimesheetFileName = TestConstants.TimesheetFileName;
+      appOptions.AiDiagnosticMode = TestConstants.AiDiagnosticMode;
+      appOptions.AiName = TestConstants.AiName;
+    });
     ServiceProvider = c.BuildServiceProvider();
   }
 
@@ -37,7 +56,7 @@ public sealed class Test1
   [DataRow("sample3.json")]
   [DataRow("sample4.json")]
   [DataRow("sample5.json")]
-  public void TestMethod1(string sample)
+  public void TimesheetControllerTest(string sample)
   {
     var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Samples", sample);
     
@@ -62,29 +81,70 @@ public sealed class Test1
       
     });
     Assert.IsNotNull(response);
-    TimesheetController.Invoke(ServiceProvider.GetRequiredService<IFileStorage>(), ServiceProvider.GetRequiredService<ILogger>(), response);
-    Assert.AreEqual(Constants.TimesheetControllerConstants.CsvHeader, FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][0]);
-    switch (fileName)
+    TimesheetController.Invoke(ServiceProvider.GetService<IFileStorage>(), ServiceProvider.GetService<ILogger>(), ServiceProvider.GetService<IOptions<AppOptions>>(), response);
+    Assert.Contains(TestConstants.TimesheetFileName, FileStorage.Storage.Keys);
+    Assert.AreEqual(Constants.TimesheetController.CsvHeader, FileStorage.Storage[TestConstants.TimesheetFileName][0]);
+    switch (sample)
     {
       case "sample1.json":
-        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",0.75,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][1]);
+        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",0.75,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[TestConstants.TimesheetFileName][1]);
         break;
       case "sample2.json":
-        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",-0.7,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][1]);
+        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",-0.7,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[TestConstants.TimesheetFileName][1]);
         break;
       case "sample3.json":
-        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",3.0,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][1]);
+        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",3,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[TestConstants.TimesheetFileName][1]);
         break;
       case "sample4.json":
-        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",-3.0,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][1]);
+        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды\",-3,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[TestConstants.TimesheetFileName][1]);
         break;
       case "sample5.json":
-        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды написать тесты\",-3.0,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[Constants.TimesheetControllerConstants.CsvFileName][1]);
+        Assert.AreEqual("2026.09.09 19:08:59,\"Реализовать сервис получения погоды написать тесты\",-3,https://tfs.company.ru/web/wi.aspx?pcguid=70c2d8cc-553f-4819-8ba2-e56afc3bf040&id=358448,company\\User,MyProject", FileStorage.Storage[TestConstants.TimesheetFileName][1]);
         break;
       default:
         break;
     }
-    Assert.IsFalse(FileStorage.Storage.ContainsKey("Log"));
+    Assert.IsFalse(FileStorage.Storage.ContainsKey(TestConstants.LogFileName));
+  }
+
+  [TestMethod]
+  [DataRow("comment_sample1.json")]
+  [DataRow("comment_sample2.json")]
+  public void AiControllerTest(string sample)
+  {
+    var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Samples", sample);
+    
+    var file = new FileInfo(fileName);
+    if (file == null)
+    {
+      throw new FileNotFoundException("Test file not found", fileName);
+    }
+
+    string content = string.Empty;
+    if (file.Exists)
+    {
+      using var rfs = file.OpenRead();
+      var readBuffer = new byte[file.Length];
+      rfs.ReadAsync(readBuffer, 0, readBuffer.Length).GetAwaiter().GetResult();
+      content = Encoding.UTF8.GetString(readBuffer);
+    }
+    Assert.IsNotEmpty(content);
+    var response = JsonSerializer.Deserialize<TfsResponseComment>(content, new JsonSerializerOptions()
+    {
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    });
+
+    Assert.IsNotNull(response);
+    AiController.Invoke(ServiceProvider.GetService<IFileStorage>(), ServiceProvider.GetService<ILogger>(), ServiceProvider.GetService<IOptions<AppOptions>>(), response);
+    var result = FileStorage.Storage[TestConstants.LogFileName][0];
+    switch (sample)
+    {
+      case "comment_sample1.json":
+        Assert.AreEqual("2", result);
+        break;
+      default:
+        break;
+    }
   }
 }
 
@@ -121,6 +181,6 @@ class Logger : ILogger
 
   public void Log(string text, string level, string method)
   {
-    _storage.Save("Log", $"[{level}] [{method}] {text}");
+    _storage.Save(TestConstants.LogFileName, $"[{level}] [{method}] {text}");
   }
 }
--- Yarkov.Tfs.Webhook.Test/test.ps1
+++ Yarkov.Tfs.Webhook.Test/test.ps1
@@ -4,5 +4,8 @@ param (
   $SampleFile
 )
 
-# Get-Content Samples/${SampleFile}.json -Encoding UTF8
-Invoke-WebRequest -Method POST http://localhost:5132/timesheet -ContentType "application/json; charset=utf-8" -Body (Get-Content "Samples/${SampleFile}.json" -Encoding UTF8)
+if ($SampleFile -like "comment*") {
+  Invoke-WebRequest -Method POST http://localhost:5132/ai -ContentType "application/json; charset=utf-8" -Body (Get-Content "Samples/${SampleFile}.json" -Encoding UTF8)
+} else {
+  Invoke-WebRequest -Method POST http://localhost:5132/timesheet -ContentType "application/json; charset=utf-8" -Body (Get-Content "Samples/${SampleFile}.json" -Encoding UTF8)
+}
